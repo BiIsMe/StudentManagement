de61b3a1c6b178d03ebd454a6eef32b0615381ea
emcudidi125@gmail.com <emcudidi125@gmail.com>
2021-07-10 16:46:20 +0700

    update security interceptor

---
+++ src/main/java/com/poly/InterConfig.java
@@ -0,0 +1,28 @@
+package com.poly;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class InterConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	SecurityInterceptor auth;
+
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {		
+		
+		registry.addInterceptor(auth)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/login");
+		
+
+		
+	}
+
+}
